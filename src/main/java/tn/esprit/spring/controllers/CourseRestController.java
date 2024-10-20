5b249d21ca753e0352ae9c3e485cb00ddc6e7fae
rihemRD <rihem.drissi@esprit.tn>
2024-10-20 18:47:00 +0100

    Ajout de logs et tests unitaires pour le module Course

--- src/main/java/tn/esprit/spring/controllers/CourseRestController.java
+++ src/main/java/tn/esprit/spring/controllers/CourseRestController.java
@@ -3,24 +3,31 @@ package tn.esprit.spring.controllers;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.LogManager;
 import org.springframework.web.bind.annotation.*;
 import tn.esprit.spring.entities.Course;
 import tn.esprit.spring.entities.TypeCourse;
 import tn.esprit.spring.services.ICourseServices;
 
+import javax.validation.Valid;
 import java.util.List;
+import java.util.logging.Logger;
 
 @Tag(name = "\uD83D\uDCDA Course Management")
 @RestController
 @RequestMapping("/course")
 @RequiredArgsConstructor
+@Slf4j
 public class CourseRestController {
     
     private final ICourseServices courseServices;
+    private static final Logger logger = Logger.getLogger(CourseRestController.class.getName());
 
     @Operation(description = "Add Course")
     @PostMapping("/add")
-    public Course addCourse(@RequestBody Course course){
+    public Course addCourse(@Valid @RequestBody Course course){
+        logger.info("Ajout d'un nouveau cours : " + course.getNumCourse());
         return  courseServices.addCourse(course);
     }
 
@@ -33,6 +40,7 @@ public class CourseRestController {
     @Operation(description = "Update Course ")
     @PutMapping("/update")
     public Course updateCourse(@RequestBody Course course){
+        logger.info("update d'un cours : " + course.getNumCourse());
         return  courseServices.updateCourse(course);
     }
 
--- src/test/java/tn/esprit/spring/services/CourseServiceTest.java
+++ src/test/java/tn/esprit/spring/services/CourseServiceTest.java
@@ -1,4 +1,45 @@
 package tn.esprit.spring.services;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import tn.esprit.spring.entities.Course;
+import tn.esprit.spring.entities.Registration;
+import java.util.HashSet;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static tn.esprit.spring.entities.Support.SKI;
+import static tn.esprit.spring.entities.TypeCourse.INDIVIDUAL;
+
+@SpringBootTest
 public class CourseServiceTest {
+
+    @Autowired
+    private ICourseServices courseServices;
+
+    @Test
+    public void testAddCourse() {
+        // Créez une instance de Registration
+        Registration registration = new Registration();
+
+        // Créez un Set de Registration et ajoutez-y votre instance
+        Set<Registration> registrations = new HashSet<>();
+        registrations.add(registration);
+
+        // Créez un objet Course avec le Set de Registration
+        Course course = new Course(1l, 1, INDIVIDUAL, SKI, 20.5f, 2, registrations);
+
+        // Ajoutez le cours en appelant la méthode du service
+        Course addedCourse = courseServices.addCourse(course);
+
+        // Assurez-vous que le cours ajouté n'est pas nul
+        assertNotNull(addedCourse);
+
+        // Vérifiez que le cours ajouté a le bon numéro de cours (par exemple)
+        assertEquals(1l, addedCourse.getNumCourse().longValue());
+    }
 }
+

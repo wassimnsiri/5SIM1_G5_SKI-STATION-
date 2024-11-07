d8e8671e8dca488cc7d5f0f15f34df2e4bbea0c5
wassimnsiri <73969384+wassimnsiri@users.noreply.github.com>
2024-11-07 23:12:31 +0100

    Update ISubscriptionRepository.java

--- src/main/java/tn/esprit/spring/repositories/ISubscriptionRepository.java
+++ src/main/java/tn/esprit/spring/repositories/ISubscriptionRepository.java
@@ -22,8 +22,9 @@ public interface ISubscriptionRepository extends CrudRepository<Subscription, Lo
     List<Subscription> findDistinctOrderByEndDateAsc();
 
 
-    @Query("select (sum(s.price))/(count(s)) from Subscription s where s.typeSub = ?1")
+    @Query("select COALESCE(sum(s.price) / count(s), 0) from Subscription s where s.typeSub = ?1")
     Float recurringRevenueByTypeSubEquals(TypeSubscription typeSub);
+
 
 
 }

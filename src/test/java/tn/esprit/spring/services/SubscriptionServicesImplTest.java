2e68ddbd1c1cdf959f776ae773b51c95560dcceb
saifmessaoudi <saif.messaoudi@esprit.tn>
2024-11-10 21:42:18 +0100

    test

--- src/test/java/tn/esprit/spring/services/SubscriptionServicesImplTest.java
+++ src/test/java/tn/esprit/spring/services/SubscriptionServicesImplTest.java
@@ -1,48 +1,51 @@
 package tn.esprit.spring.services;
 
-import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
 import tn.esprit.spring.entities.Subscription;
 import tn.esprit.spring.entities.TypeSubscription;
 import tn.esprit.spring.repositories.ISubscriptionRepository;
 
 import java.time.LocalDate;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.ArgumentMatchers.any;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-@Slf4j
-
 class SubscriptionServicesImplTest {
 
 
     @Mock
-    private ISubscriptionServices iSubscriptionServices;
+    private ISubscriptionRepository subscriptionRepository;
 
     @InjectMocks
-    private Subscription subscription = new Subscription( 1L, LocalDate.now(), LocalDate.now().plusMonths(1), 100.0f, TypeSubscription.MONTHLY);
+    SubscriptionServicesImpl subscriptionServices;
 
+    private Subscription subscription;
 
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
+        subscription = new Subscription();
+        subscription.setNumSub(1L);
+        subscription.setStartDate(LocalDate.now());
+        subscription.setEndDate(LocalDate.now().plusMonths(1));
+        subscription.setPrice(100.0f);
+        subscription.setTypeSub(TypeSubscription.valueOf("ANNUAL"));
     }
 
     @Test
     void addSubscription() {
-        when(iSubscriptionServices.addSubscription(any(Subscription.class))).thenReturn(subscription);
-        Subscription subscription1 = iSubscriptionServices.addSubscription(subscription);
-        assertNotNull(subscription1);
-        verify(iSubscriptionServices, times(1)).addSubscription(subscription);
+        when(subscriptionRepository.save(Mockito.any(Subscription.class))).thenReturn(subscription);
+        Subscription subscription1 = subscriptionServices.addSubscription(subscription);
+        assertThat(subscription1).isNotNull();
+        verify(subscriptionRepository).save(Mockito.any(Subscription.class));
     }
 
 

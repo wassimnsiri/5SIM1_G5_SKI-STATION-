b8d493b2790cc54a068510229222d9891c0d8523
saifmessaoudi <saif.messaoudi@esprit.tn>
2024-11-10 21:46:05 +0100

    test

--- src/test/java/tn/esprit/spring/services/SubscriptionServicesImplTest.java
+++ src/test/java/tn/esprit/spring/services/SubscriptionServicesImplTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import tn.esprit.spring.entities.Subscription;
 import tn.esprit.spring.entities.TypeSubscription;
